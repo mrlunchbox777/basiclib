8f3d5c5cc28f21f463c56975580011b3afa930a0
Andrew Shoell <mrlunchbox777@gmail.com>
2023-10-31 19:25:18 -0600

    adding a few more tests

--- pkg/os/file_test.go
+++ pkg/os/file_test.go
@@ -34,6 +34,19 @@ func TestReadArbitraryFile(t *testing.T) {
 	Cleanup()
 }
 
+// TestReadArbitraryFilePanic tests that ReadFile panics on error
+func TestReadArbitraryFilePanic(t *testing.T) {
+	Cleanup()
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("ReadArbitraryFile(%s) should have panicked", path)
+		}
+	}()
+
+	ReadArbitraryFile(path)
+}
+
 // TestReadTextFile tests that ReadTextFile returns the contents of a text file
 func TestReadTextFile(t *testing.T) {
 	Cleanup()
@@ -46,6 +59,19 @@ func TestReadTextFile(t *testing.T) {
 		t.Errorf("ReadTextFile(%s) = %s, didn't return %s", path, got, content)
 	}
 	Cleanup()
+}
+
+// TestReadTextFilePanic tests that ReadTextFile panics on error
+func TestReadTextFilePanic(t *testing.T) {
+	Cleanup()
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("ReadTextFile(%s) should have panicked", path)
+		}
+	}()
+
+	ReadTextFile(path)
 }
 
 // TestReadFile tests that ReadFile returns the contents of a text file
@@ -62,6 +88,19 @@ func TestReadFile(t *testing.T) {
 	Cleanup()
 }
 
+// TestReadFilePanic tests that ReadFile panics on error
+func TestReadFilePanic(t *testing.T) {
+	Cleanup()
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("ReadFile(%s) should have panicked", path)
+		}
+	}()
+
+	ReadFile(path)
+}
+
 // TestWriteFile tests that WriteFile writes data to a file
 func TestWriteFile(t *testing.T) {
 	Cleanup()
@@ -76,6 +115,22 @@ func TestWriteFile(t *testing.T) {
 	Cleanup()
 }
 
+// TestWriteFilePanic tests that WriteFile panics on error
+func TestWriteFilePanic(t *testing.T) {
+	Cleanup()
+	badPath := "/this/is/a/bad/path"
+	content := math.RandomString(10)
+	perms := 0644
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("WriteFile(%s, %s, %d) should have panicked", badPath, content, perms)
+		}
+	}()
+
+	WriteFile(badPath, []byte(content), os.FileMode(perms))
+}
+
 // Test Remove tests that Remove removes a file
 func TestRemove(t *testing.T) {
 	Cleanup()
@@ -88,6 +143,20 @@ func TestRemove(t *testing.T) {
 	if !os.IsNotExist(err) {
 		t.Errorf("Remove(%s) didn't remove the file", path)
 	}
+}
+
+// TestRemovePanic tests that Remove panics on error
+func TestRemovePanic(t *testing.T) {
+	Cleanup()
+	badPath := "/this/is/a/bad/path"
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Remove(%s) should have panicked", badPath)
+		}
+	}()
+
+	Remove(badPath)
 }
 
 // TestExists tests that Exists returns true if a file exists
